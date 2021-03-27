c0096208a955eb66970f6cbdeebfcd1bc650f19b
Esteban <eacevedo@hightech-corp.com>
2021-03-27 16:38:13 -0600

    cambiando de visual basic a c#

--- voting-application/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="voting_application.MvcApplication" Language="VB" %>
---
+++ voting-application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace voting_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
