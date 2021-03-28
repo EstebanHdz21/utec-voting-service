0b611fff59f671cb459faf8b87b516e4ea539cfd
Carlos Ernesto <cmendoza@hightech-corp.com>
2021-03-27 18:17:08 -0600

    bibliotecas de clases agregados

---
+++ BL/RegistroNacionalDAL.cs
@@ -0,0 +1,13 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    class RegistroNacionalDAL
+    {
+        Connection conn = new Connection();
+
+    }
+}
